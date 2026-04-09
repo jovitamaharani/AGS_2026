86b95bfa4186f16ca4c016fc2c001df70ea2089a
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2026-04-09 21:31:51 +0700

    p1_percobaan1

--- src/p6/SortingMain17.java
+++ src/p6/SortingMain17.java
@@ -3,12 +3,20 @@ package p6;
 public class SortingMain17 {
     public static void main(String[] args) {
         int a[] = { 20, 10, 2, 7, 12 };
+        int b[] = { 30, 20, 2, 8, 14 };
         Sorting17 dataurut1 = new Sorting17(a, a.length);
+        Sorting17 dataurut2 = new Sorting17(b, b.length);
 
         System.out.println("Data awal 1");
         dataurut1.tampil();
         dataurut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil(); 
     }
 }
--- src/p6/laporan.md
+++ src/p6/laporan.md
@@ -6,3 +6,20 @@
 | Repository | [link] (https://github.com/jovitamaharani/AGS_2026/tree/main/src/p6) |
 
 # Jobsheet 5 #5 SORTING (BUBBLE, SELECTION, DAN INSERTION SORT)
+
+## 5.2 Praktikum 1 - Mengimplementasikan Sorting menggunakan object
+a. SORTING – BUBBLE SORT
+```
+Data awal 1
+20 10 2 7 12 
+Data sudah diurutkan dengan BUBBLE SORT (ASC)
+2 7 10 12 20 
+```
+b. SORTING – SELECTION SORT
+```
+Data awal 2
+30 20 2 8 14
+Data sudah diurutkan dengan SELECTION SORT (ASC)
+2 8 14 20 30
+
+```
