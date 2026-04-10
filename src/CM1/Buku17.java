b921f9061649c1e9f9f30b064562aa9d36f8cc9e
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2026-04-10 10:38:36 +0700

    CM1_class buku

---
+++ src/CM1/Buku17.java
@@ -0,0 +1,16 @@
+package CM1;
+
+public class Buku17 {
+    String kodeBuku, judul;
+    int tahunTerbit;
+
+    public Buku17(String kode, String judul, int tahun) {
+        this.kodeBuku = kode;
+        this.judul = judul;
+        this.tahunTerbit = tahun;
+    }
+
+    public void tampilBuku() {
+        System.out.println("Kode: " + kodeBuku + " | Judul: " + judul + " | Tahun: " + tahunTerbit);
+    }
+}
